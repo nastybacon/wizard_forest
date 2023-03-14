4f12a0449d6d23372a1fc1c0fcf6566bb9ff8d3e
nastybacon <126672145+nastybacon@users.noreply.github.com>
2023-03-14 15:14:04 +0100

    01 - Canvas

---
+++ game.js
@@ -0,0 +1,32 @@
+/**
+ * OBJEKTE ERSTELLEN
+ * 
+ * ----- 01 - Canvas -----
+ * -> 2D Welt + Elemente Links und Rechts bewegen
+ * -> JS hat dafür ein Baukasten - Canvas - ähnliches, wie ein Container
+ * -> hat automatisch eine X + Y Achse -> BILD 1.1
+ * -> wir können an gewissen Koordinaten Bilder hinzufügen
+ * 
+ * -> Man kann z.B. ganz rechts ein Bild einfügen und für eine ms das Bild verschwinden lassen
+ * -> schwar + fügen ein Bild ein + schwarz + fügen ein Bild ein usw
+ * -> das geht so schnell, dass man das schwarze nicht mehr sieht
+ * -> der Punkt (0,0) liegt links oben, nicht, wie bei Mathe BILD 1.2
+ * 
+ * -> zwei Bilder hintereinander einfügen - sieht aus, als würde er laufen
+ * 
+ * -> zwei Varianten verlinkt - Hai wäre unter Wasser
+ * -> man kann ein Rahmen zeichnen mit Koordinaten und das Bild da einfügen + werden wir viel üben
+ * -> BILD 1.3
+ * -> man kan graphisch auch mehrere Elemente hinzufügen - Hintergrundbild
+ * -> dieser besteht aus mehreren Schrichten und diese bewegen sich
+ * 
+ *  ----- 1.1. - Alternative Grafiken -----
+ * 
+ * -> https://craftpix.net/
+ * -> wir müssen nicht die gegebenen Graphiken nehmen - können auch eigenes aussuchen
+ * -> Lizenz - kann man bei der Seite verwenden
+ * -> verändern + verkaufen
+ * -> wir brauche Elemente vom BILD 2.1, 2.2 uns 2.3 -> Entgegner
+ * 
+ * 
+ */
