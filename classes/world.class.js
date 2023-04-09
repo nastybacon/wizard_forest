b41c17b0ce87eed144bcb3a459a6fb86d10ec10e
nastybacon <126672145+nastybacon@users.noreply.github.com>
2023-04-09 20:45:11 +0200

    requestAnimationFrame()

--- classes/world.class.js
+++ classes/world.class.js
@@ -6,14 +6,29 @@ class World {
         new Enemy(),
         new Enemy(),
     ];
+    canvas;
     ctx;
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
     draw() {
+
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // Objekt wieder löschen
+
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        this.enemies.forEach(enemy => {
+
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+
+        });
+
+        let self = this; // alter Hack bei OO
+        requestAnimationFrame(function() {
+            self.draw(); // wird so häufig aufgerufen, wie die Grafikkarte es zulässt
+        });
     }
 }
